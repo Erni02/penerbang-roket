837a8e92ffddef75f993664b6a3a0ea3cced67b3
Erni <92587836+Erni02@users.noreply.github.com>
2021-10-19 11:53:17 +0800

    Menambahkan file
    Program sederhana

---
+++ tugas program sederhana erni.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	system("title program penjumlahan sederhana");
+	cout<<"Nama     : Erni"<<endl;
+	cout<<"Nim      : F1G121053"<<endl;
+	cout<<"Jurusan  : Ilmu Komputer"<<endl;
+	cout<<"Fakultas : MIPA"<<endl;
+	
+	//Operator Penjumlahan Erni
+	int angka1,angka2,hasil;
+	cout<<"Angka Pertama = ";
+	cin>>angka1;
+	cout<<"Angka Kedua = ";
+	cin>>angka2;
+	hasil = angka1 + angka2;
+	cout<<"Hasil Penjumlahan = "<<angka1+angka2<<endl;
+	
+	return 0;
+	
+}
